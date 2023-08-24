0236ff7e932aba0429f0dc346c380fffe70f59ff
Lauane Cardoso <lauaneccardoso@gmail.com>
2023-08-24 01:45:18 +0100

    atualizacao api

--- Desafio_F1/Dados_API.py
+++ Desafio_F1/Dados_API.py
@@ -1,11 +1,27 @@
 import requests
-import json 
-import pandas as pd 
+import pandas as pd
+from flatten_json import flatten
 
+base_url = "https://ergast.com/api/f1/2021/results.json"
+limit = 40
+offset = 0
+all_data = []
 
-dadosf1 = requests.get('http://ergast.com/api/f1/2021.json')
-print(dadosf1)
+while True:
+    url = f"{base_url}?limit={limit}&offset={offset}"
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        data = response.json()
 
-content_type = dadosf1.headers.get('content-type')
-print(content_type)
+        if len(data["MRData"]["RaceTable"]["Races"]) == 0:
+            break 
+            
+        all_data.extend(data["MRData"]["RaceTable"]["Races"])
+        offset += limit
+    else:
+        print(f"Erro na requisição: {response.status_code}")
+        break
 
+flattened_data = [flatten(item) for item in all_data]
+df = pd.DataFrame(flattened_data)
